ff4e35d3441e616945dbe653c63870b7b119c3e8
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-06-01 18:08:27 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { NavBar } from './components/navbar/Navbar';
 import { Banner } from './components/banner/Banner';
+import { Skills } from './components/skills/Skills';
+import { Projects } from './components/projects/Projects';
+import { Contact } from './components/contact/Contact';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
